105d496a554866465e9ef759e03698a9dd1ab39c
Kethan VA <kethan@netreo.com>
2020-11-18 18:22:04 +0530

    Changes made for CICD

--- 1-build-layer1.sh
+++ 1-build-layer1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 rm -rf ./layer1
-cd sample_code1
-/Library/Frameworks/Python.framework/Versions/3.8/bin/pip3 install --target ../layer1/python -r requirements.txt
+/Library/Frameworks/Python.framework/Versions/3.8/bin/pip3 install --target ../layer1/python -r sample_code1/requirements.txt
