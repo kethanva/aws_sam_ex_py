7045adc71ea80ecec017d8fb90e6cac0169c4dec
Kethan VA <kethan@netreo.com>
2020-11-18 18:48:19 +0530

    Fixed some build script

--- 1-build-layer1.sh
+++ 1-build-layer1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 rm -rf ./layer1
-/Library/Frameworks/Python.framework/Versions/3.8/bin/pip3 install --target ../layer1/python -r sample_code1/requirements.txt
+/Library/Frameworks/Python.framework/Versions/3.8/bin/pip3 install --target ./layer1/python -r sample_code1/requirements.txt
